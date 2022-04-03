6debabfc916d937deab526fb6c5e48f92953f365
Аня Сусак <anna.i.susak@gmail.com>
2022-04-03 22:19:16 +0300

    add save 1

--- task_checker.py
+++ task_checker.py
@@ -4,21 +4,33 @@ from utils import db_action, DBAction
 class Task:
     id: int
     name: str
+    description: str
     output: str
 
-    def __init__(self, task_id: int, name: str, output: str):
+    def __init__(self, task_id: int, name: str, description: str, output: str):
         self.id = task_id
         self.name = name
+        self.description = description
         self.output = output
+
+    def save(self, name_val: str, description_val: str, output_val: str):
+        db_action(
+            '''
+            update tasks set name=?, description=?, output=? where id=?
+            ''',
+            (name_val, description_val, output_val, self.id),
+            DBAction.fetchone
+        )
 
 
 def get_task(task_id: int) -> Task:
     db_task = db_action(
         '''
         select * from tasks where id=?
-        '''
-        (task_id, ),
+        ''',
+        (task_id,),
         DBAction.fetchone
     )
+
     task = Task(db_task[0], db_task[1], db_task[2], db_task[3])
     return task
